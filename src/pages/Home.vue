abfcae084d2515ee4dba8b5562a223e7692be2e7
William G. Tresselt <williagt@stud.ntnu.no>
2022-04-21 08:45:50 +0200

    Looking at sorting component.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import Card from '../components/Card.vue'
 import Chat from "./Chat.vue";
+import BaseInput from '../components/form/BaseInput.vue'
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+let sortChosen = 6
+let sortAlts:Array<object> = [{id:1, alt:'Pris lav-høy'}, {id:2, alt:'Pris høy-lav'},
+  {id:3, alt:'Eldste først'}, {id:4, alt:'Nyeste først'}, {id:5, alt:'Nærmest'}, {id:6, alt:'Ingen sortering'}]
+
+let tags:Array<string> = []
 
 let testArray:Array<string> = ['hei','på','deg']
 </script>
@@ -13,6 +20,7 @@ let testArray:Array<string> = ['hei','på','deg']
 
   <div>
     <!--Text search input component-->
+    <BaseInput></BaseInput>
     <div>search</div>
   </div>
 
@@ -34,8 +42,10 @@ let testArray:Array<string> = ['hei','på','deg']
   <div>
     <!--Sorting component-->
     <div class="overflow-y-auto grid gap-4">
-      <div v-for="t in testArray">
-        <input type="radio" :value="t"> {{ t }}
+      {{sortChosen}}
+      <div v-for="alt in sortAlts">
+        <input type="radio" :value="alt.alt" :id="alt.id" name="sort" v-model="sortChosen">
+        <label :for="alt.id">{{ alt.alt }}</label>
       </div>
     </div>
 
