834c2096ccd41681369afa285e598ca5a28262df
Erlend Ryan <erlendryan@gmail.com>
2022-04-21 09:21:37 +0200

    typescript refactor for message container

--- src/components/chat/MessageContainer.vue
+++ src/components/chat/MessageContainer.vue
@@ -1,42 +1,42 @@
 <script setup lang="ts">
 import Card from '../components/Card.vue'
-import {PropType, ref} from "vue";
-import Message from "./Message.vue";
-import Receipt from "./Receipt.vue";
+import { PropType, ref } from 'vue'
+import Message from './Message.vue'
+import Receipt from './Receipt.vue'
 
 interface IMessage {
-  message: String,
-  date: Date,
-  receive: Boolean,
+	message: String
+	date: Date
+	receive: boolean
+}
+
+interface Props {
+	messages: IMessage[]
 }
 
 const receipt = ref({
-  username: "Bruker",
-  itemName: "Fiskestang",
-  loanStart: new Date().toDateString(),
-  loanStop: new Date().toDateString(),
-  itemPrice: 1000,
-  itemUnit: "dag",
+	username: 'Bruker',
+	itemName: 'Fiskestang',
+	loanStart: new Date().toDateString(),
+	loanStop: new Date().toDateString(),
+	itemPrice: 1000,
+	itemUnit: 'dag',
 })
 
-
-const props =  defineProps({
-  messages: {
-    type: Array as PropType<Array<IMessage>>,
-    default: () => [],
-    required: true,
-  },
-})
-
-
+const { messages } = defineProps<Props>()
 </script>
 <template>
-  <div class="bg-F7F9FC-100 border border-black-400 px-4 py-3 rounded md:container mx-auto h-full min-h-screen">
-
-    <Message v-for="(message, i) in messages" :id="i" :receive="message.receive">
-      <div>{{ message.message }}</div>
-    </Message>
-    <receipt :receipt="receipt"></receipt>
-    <receipt :receipt="receipt" :receive="true"></receipt>
-  </div>
+	<div
+		class="bg-F7F9FC-100 border border-black-400 px-4 py-3 rounded md:container mx-auto h-full min-h-screen"
+	>
+		<Message
+			v-for="(message, i) in messages"
+			:id="i"
+			:receive="message.receive"
+		>
+			<div>{{ message.message }}</div>
+		</Message>
+		<receipt :receipt="receipt"></receipt>
+		<receipt :receipt="receipt" :receive="true"></receipt>
+	</div>
 </template>
