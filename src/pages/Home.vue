6de3408ce5cf9e0c9015b0f67aa289b46e1ad560
William G. Tresselt <williagt@stud.ntnu.no>
2022-04-21 10:46:01 +0200

    Added bare bone TagList.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,13 +2,57 @@
 import Card from '../components/Card.vue'
 import Chat from "./Chat.vue";
 import BaseInput from '../components/form/BaseInput.vue'
+import axios from 'axios'
+import {ref} from 'vue'
+import Tag from "../components/TagList.vue";
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
+function search() {
+  axios.get("/item")
+}
+function categoryChosen(tag:Category) {
+  chosenTags.value.push(tag)
+  tagAlts.value.forEach((value, index) => {
+    if(value.id==tag.id) tagAlts.value.splice(index, 1)
+  })
+}
+function categoryRemoved(tag:Category) {
+  tagAlts.value.push(tag)
+  chosenTags.value.forEach((value, index) => {
+    if(value.id==tag.id) chosenTags.value.splice(index, 1)
+  })
+}
+function getAllSuperTags() {
+  axios.get("/url-to-get-all-super-tags")
+      .then(response => {
+        tagAlts = response.data
+      })
+}
+interface ItemListing {
+  id: number,
+  ownerId: number,
+  categoryId: number,
+  active: boolean,
+  name: string,
+  price: number,
+  priceUnit: string,
+  displayPhoneNumber: boolean,
+  address: string,
+  postalCode: string
+}
+interface Category {
+  id: number,
+  superCategoryId: number,
+  name: string
+}
 let sortChosen = 6
 let sortAlts:Array<object> = [{id:1, alt:'Pris lav-høy'}, {id:2, alt:'Pris høy-lav'},
   {id:3, alt:'Eldste først'}, {id:4, alt:'Nyeste først'}, {id:5, alt:'Nærmest'}, {id:6, alt:'Ingen sortering'}]
 
-let tags:Array<string> = []
+let searchWord = ref<string>("")
+let tagAlts = ref<Array<Category>>([{id: 1, superCategoryId: 0, name:'some'}, {id: 2, superCategoryId: 0, name:'dummy'}, {id: 3, superCategoryId: 0, name:'values'}])
+let chosenTags = ref<Array<Category>>([{id: 4, superCategoryId: 0, name:'hola'}])
+let items:Array<ItemListing> = []
 
 let testArray:Array<string> = ['hei','på','deg']
 </script>
@@ -22,14 +66,15 @@ let testArray:Array<string> = ['hei','på','deg']
     <!--Text search input component-->
     <BaseInput></BaseInput>
     <div>search</div>
+    <input type="text" v-model="searchWord" placeholder="Søk">
+    Ooga {{searchWord}}
   </div>
 
   <div>
     <!--Tag input component-->
     <div>categories goes here</div>
-    <div class="overflow-x-auto flex gap-4">
-      <div v-for="t in testArray">{{t}}</div>
-    </div>
+    <Tag v-model="chosenTags" :removable="true" @remove-tag-event="categoryRemoved"></Tag>
+    <Tag v-model="tagAlts" @add-tag-event="categoryChosen"></Tag>
   </div>
 
   <div>
