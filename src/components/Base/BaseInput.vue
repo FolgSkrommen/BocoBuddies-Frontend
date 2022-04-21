41eba6229d4ce96f0a7219ae9017e1d025b21f00
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-04-21 11:17:02 +0200

    Added: register and errormessage for base input

--- src/components/Base/BaseInput.vue
+++ src/components/Base/BaseInput.vue
@@ -4,6 +4,7 @@ const props = defineProps({
 	label: String,
 	disabled: Boolean,
 	type: String,
+  error: String,
 })
 
 const emit = defineEmits(['update:modelValue'])
@@ -17,12 +18,14 @@ const updateValue = (event: Event) => {
 	<div class="flex flex-col">
 		<label class="text-slate-500 p-2">{{ label }}</label>
 		<input
-			class="bg-slate-100 rounded-xl py-1 px-2 shadow-lg"
+			class="bg-slate-100 rounded-xl py-2 px-2 shadow-lg"
 			:class="disabled ? 'bg-gray-300' : ''"
 			:disabled="disabled"
 			:value="modelValue"
 			@input="updateValue"
 			:type="type ?? 'text'"
 		/>
+
+    <p class=" text-red-500 text-xs"> {{error}} </p>
 	</div>
 </template>
