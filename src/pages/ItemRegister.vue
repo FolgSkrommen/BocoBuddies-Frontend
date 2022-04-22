1346880beb60eeb58cc302352a5ac183eb3a0241
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-04-22 18:25:23 +0200

    Added: dynamic category selection

--- src/pages/ItemRegister.vue
+++ src/pages/ItemRegister.vue
@@ -1,66 +1,69 @@
 <script setup lang="ts">
-import BaseInput from '../components/base/BaseInput.vue'
-import { store } from '../store'
+//Components
+import BaseInput from '../components/Base/BaseInput.vue'
+import BaseButton from '../components/Base/BaseBtn.vue'
+import BaseDropdown from '../components/Base/BaseDropdown.vue'
 
+import { store } from '../store'
+import axios from 'axios'
+
+import { computed, onMounted, Ref, ref } from 'vue'
+
+//Form validation
+// /*
 import { useForm, useField } from 'vee-validate'
 import * as yup from 'yup'
-import axios from 'axios'
-import BaseButton from '../components/base/BaseBtn.vue'
-import { ref, computed } from 'vue'
 
 const schema = yup.object({
-	username: yup.string().required('Brukernavn er påkrevd'),
-	firstName: yup.string().required('Fornavn er påkrevd'),
-	lastName: yup.string().required('Etternavn er påkrevd'),
-	email: yup.string().required('Epost er påkrevd').email('Ikke gyldig'),
+	title: yup.string().required('Brukernavn er påkrevd'),
+	description: yup.string().required('Fornavn er påkrevd'),
+	price: yup.string().required('Etternavn er påkrevd'),
+	category: yup.string().required('Epost er påkrevd').email('Ikke gyldig'),
 	address: yup.string().required('Adresse er påkrevd'),
 	postalcode: yup.string().required('Postnummer er påkrevd').min(4),
-	phonenumber: yup.number().required('Telefon er påkrevd').min(8),
-	password: yup.string().required('Passord er påkrevd').min(8),
 })
-// Create a form context with the validation schema
 const { errors } = useForm({
 	validationSchema: schema,
 })
-// No need to define rules for fields
-let { value: username } = useField('username')
-let { value: firstName } = useField('firstName')
-let { value: lastName } = useField('lastName')
-let { value: email } = useField('email')
+let { value: title } = useField('title')
+let { value: description } = useField('description')
+let { value: price } = useField('price')
+let { value: category } = useField('category')
 let { value: address } = useField('address')
 let { value: postalcode } = useField('postalcode')
 let { value: phonenumber } = useField('phonenumber')
-let { value: password } = useField('password')
 
-let passwordCheck = ''
-let passwordIsSame = computed(() => password.value == passwordCheck)
+function submit() {}
 
-function submit() {
+/*  Categories*/
+
+interface Category {
+	name: String
+	id: number
+	superCategoryId?: number
+}
+let categoryChoices: Ref<Array<Category[]>> = ref([])
+
+onMounted(() => {
+	axios.get('/category/main').then(response => {
+		categoryChoices.value.push(response.data)
+	})
+})
+
+function update(categoryId: number, index: number) {
+	categoryChoices.value = categoryChoices.value.slice(0, index + 1)
 	axios
-		.post('/user/register', null, {
+		.get('/category/sub', {
 			params: {
-				firstName: firstName.value,
-				lastName: lastName.value,
-				email: email.value,
-				address: address.value,
-				postalcode: postalcode.value,
-				phonenumber: phonenumber.value,
-				password: password.value,
+				categoryId: categoryId,
 			},
 		})
 		.then(response => {
-			store.commit('SET_USER_DATA', response.data)
+			if (response.data.length > 0) {
+				categoryChoices.value.push(response.data)
+			}
 		})
-		.catch()
 }
-
-const notValid = computed(
-	() =>
-		!!errors.value.email ||
-		!!errors.value.password ||
-		email.value == undefined ||
-		password.value == undefined
-)
 </script>
 
 <template>
@@ -69,58 +72,51 @@ const notValid = computed(
 
 		<form @submit.prevent="submit()">
 			<BaseInput
-				v-model.lazy="username"
+				v-model.lazy="title"
 				label="Tittel"
-				:error="errors.username"
+				:error="errors.title"
 			/>
 			<BaseInput
-				v-model.lazy="firstName"
+				v-model.lazy="description"
 				label="Beskrivelse"
-				:error="errors.firstName"
-			/>
-			<BaseInput
-				v-model.lazy="lastName"
-				label="Pris"
-				:error="errors.lastName"
+				:error="errors.description"
 			/>
 
 			<BaseInput
-				v-model.lazy="email"
-				label="Hovedkategori"
-				:error="errors.email"
+				v-model.lazy="price"
+				label="Pris"
+				:error="errors.price"
 			/>
-			<BaseInput
-				v-model="phonenumber"
-				label="Underkategori"
-				:error="errors.phonenumber"
-			/>
+
+			<h2>Kategorier</h2>
+			<select
+				v-for="(categories, index) in categoryChoices"
+				:key="index"
+				class="border-2 border-black rounded bg-gray-300 items-center"
+				@input="event => update(event.target.value, index)"
+			>
+				>
+				<option
+					v-for="category in categories"
+					:key="category.id"
+					:value="category.id"
+				>
+					{{ category.name }}
+				</option>
+			</select>
 
 			<BaseInput
 				v-model="address"
-				label="Addresse"
+				label="Bilder"
 				:error="errors.address"
 			/>
 			<BaseInput
 				v-model="postalcode"
-				label="Postnummer"
+				label="Adresse"
 				:error="errors.postalcode"
 			/>
 
-			<BaseInput
-				v-model="password"
-				label="Passord"
-				type="password"
-				:error="errors.password"
-			/>
-			<BaseInput
-				v-model="passwordCheck"
-				label="Gjenta passord"
-				type="password"
-			/>
-
-			<BaseButton class="m-4" type="submit" :disabled="notValid"
-				>Send</BaseButton
-			>
+			<BaseButton class="m-4" type="submit">Send</BaseButton>
 		</form>
 	</div>
 </template>
