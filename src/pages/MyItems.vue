41a78849187467d6258668f1f57f888a07e9edd1
Erlend Ryan <erlendryan@gmail.com>
2022-04-25 13:31:41 +0200

    floating button

--- src/pages/MyItems.vue
+++ src/pages/MyItems.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Card from '../components/Card.vue'
+import FloatingBtn from '../components/base/FloatingBtn.vue'
+import { PlusIcon } from '@heroicons/vue/solid'
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 </script>
 
 <template>
 	<div>
 		<h1>Mine Gjenstander</h1>
+		<FloatingBtn to="/item-register" />
 	</div>
 </template>
