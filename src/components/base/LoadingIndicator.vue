f75a77177e59f20b0250c51b4c25b7d9810f736c
Erlend Ryan <erlendryan@gmail.com>
2022-04-29 08:27:46 +0200

    random stuff

--- src/components/base/LoadingIndicator.vue
+++ src/components/base/LoadingIndicator.vue
@@ -1,7 +1,7 @@
 <template>
 	<svg
 		role="status"
-		class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-slate-200 fill-blue"
+		class="w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-slate-200 fill-blue"
 		viewBox="0 0 100 101"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
