11485de3e0c7ec3b05734c09c21bd81b7b446df1
Erlend Ryan <erlendryan@gmail.com>
2022-05-04 15:31:15 +0200

    send request

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
-const formData: PostContactRequest = {
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+import { store } from '../store'
+
+const router = useRouter()
+const data: PostContactRequest = {
 	email: '',
 	firstName: '',
 	lastName: '',
 	message: '',
 }
 
-function handleSubmit() {}
+async function handleSubmit() {
+	try {
+		await axios.post('/contact', data)
+		router.push('/')
+	} catch (error: any) {
+		store.dispatch('error', error.message)
+	}
+}
 </script>
 
 <template>
@@ -14,20 +26,20 @@ function handleSubmit() {}
 		<div class="grid gap-4 grid-cols-2">
 			<div class="grid">
 				<label>Fornavn</label>
-				<input required v-model="formData.firstName" type="text" />
+				<input required v-model="data.firstName" type="text" />
 			</div>
 			<div class="grid">
 				<label>Etternavn</label>
-				<input required v-model="formData.lastName" type="text" />
+				<input required v-model="data.lastName" type="text" />
 			</div>
 		</div>
 		<div class="grid">
 			<label>E-post</label>
-			<input required v-model="formData.email" type="email" />
+			<input required v-model="data.email" type="email" />
 		</div>
 		<div class="grid">
 			<label>Etternavn</label>
-			<textarea required v-model="formData.message" rows="6"></textarea>
+			<textarea required v-model="data.message" rows="6"></textarea>
 		</div>
 		<button type="submit" class="place-self-end">Send</button>
 	</form>
