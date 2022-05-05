0c603e3c900866a75801cc624ffe8d46359cfa78
Anders Heftøy Carlsen <anders.heftoy@gmail.com>
2022-05-05 10:36:48 +0200

    Trying to add Community tests

---
+++ tests/unit/pages/Community.spec.ts
@@ -0,0 +1,60 @@
+import { describe, expect, it, vi } from 'vitest'
+import Community from '../../../src/pages/Community.vue'
+import axios from 'axios'
+import { flushPromises, RouterLinkStub, shallowMount } from '@vue/test-utils'
+
+describe('Community', () => {
+	let mockMembers = [
+		{
+			userId: 1,
+			firstName: 'test',
+			lastName: 'test',
+			username: 'test',
+			email: 'test@test.com',
+			address: 'nardobakken 4',
+			postalCode: '7032',
+			phoneNumber: '12345678',
+			profilePicture: null,
+			friend: true,
+			hasPendingInvite: null,
+			verified: false,
+			trusted: false,
+			rating: 3,
+		},
+	]
+	describe('when entered', () => {
+		let mockFriendChats = [
+			{
+				chatId: 1,
+				chatName: 'test',
+				members: mockMembers,
+			},
+		]
+
+		vi.spyOn(axios, 'get')
+			.mockResolvedValue(mockFriendChats)
+			.mockResolvedValue(mockMembers)
+
+		it('has the required elements', async () => {
+			const mockRoute = {}
+			const mockRouter = {
+				push: vi.fn(),
+			}
+			const wrapper = shallowMount(Community, {
+				stubs: { RouterLink: RouterLinkStub },
+			})
+
+			expect(axios.get).toHaveBeenCalledTimes(1)
+			expect(axios.get).toHaveBeenCalledWith('/chat/getByUser/community')
+
+			await flushPromises()
+
+			expect(wrapper.findAll('[data-testid="view-button"]')).toHaveLength(
+				3
+			)
+
+			expect(wrapper.findAll('[data-testid="chat-card"]')).toHaveLength(1)
+			expect(wrapper.find('[data-testid="add-btn"]').exists()).toBe(false)
+		})
+	})
+})
