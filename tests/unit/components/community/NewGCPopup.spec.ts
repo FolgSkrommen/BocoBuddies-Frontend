7b2bd9be4a832a70b8c9f2592fac5936afc45fe6
ofplarsen <ofplarsen@gmail.com>
2022-05-06 17:00:34 +0200

    Added more tests

--- tests/unit/components/community/NewGCPopup.spec.ts
+++ tests/unit/components/community/NewGCPopup.spec.ts
@@ -1,7 +1,7 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import { describe, expect, it, vi } from 'vitest'
 
-import AddFriendPopup from '../../../../src/components/community-popups/AddFriendPopup.vue'
+import NewGCPopup from '../../../../src/components/community-popups/NewGCPopup.vue'
 import axios from 'axios'
 import { User } from '../../../../src/api/schema'
 
@@ -17,13 +17,13 @@ describe('AddFriendPopup', () => {
 	}
 	describe('when loaded', () => {
 		it('Component loads correctly', () => {
-			const wrapper = shallowMount(AddFriendPopup)
+			const wrapper = shallowMount(NewGCPopup)
 
 			expect(wrapper.exists()).toBe(true)
 		})
 
 		it('Loading bar loads', () => {
-			const wrapper = mount(AddFriendPopup)
+			const wrapper = mount(NewGCPopup)
 
 			expect(wrapper.find('[data-testid="loading"]').exists()).not
 			wrapper.vm.status = 'loading'
@@ -32,22 +32,25 @@ describe('AddFriendPopup', () => {
 		})
 
 		it('All elements loads correctly', () => {
-			const wrapper = mount(AddFriendPopup)
+			const wrapper = mount(NewGCPopup)
 
-			expect(wrapper.exists()).toBe(true)
 			wrapper.vm.user = mockUser
 			wrapper.vm.status = 'loaded'
 			wrapper.vm.$forceUpdate()
 			expect(wrapper.find('[data-testid="loading"]').exists()).not
 			expect(wrapper.find('[data-testid="usercard"]').exists())
-			expect(wrapper.find('[data-testid="load-more"]').exists())
+			expect(wrapper.find('[data-testid="input"]').exists())
+			expect(wrapper.find('[data-testid="user-btn"]').exists())
+			expect(wrapper.find('[data-testid="add-btn"]').exists())
+			expect(wrapper.find('[data-testid="load-btn"]').exists())
+			expect(wrapper.find('[data-testid="create-btn"]').exists())
 			expect(wrapper.find('[data-testid="searchbar"]').exists())
 		})
 	})
 
 	describe('Testing functions', () => {
 		it('searchForFriends', async () => {
-			const wrapper = shallowMount(AddFriendPopup)
+			const wrapper = shallowMount(NewGCPopup)
 
 			const spy = vi.spyOn(axios, 'get')
 
@@ -58,12 +61,12 @@ describe('AddFriendPopup', () => {
 			expect(wrapper.vm.status).toBe('error')
 		})
 
-		it('addUser', async () => {
-			const wrapper = shallowMount(AddFriendPopup)
+		it('createGC', async () => {
+			const wrapper = shallowMount(NewGCPopup)
 
 			const spy = vi.spyOn(axios, 'post')
 			wrapper.vm.$forceUpdate()
-			await wrapper.vm.addUser(mockUser)
+			await wrapper.vm.createGC()
 
 			expect(spy).toHaveBeenCalledTimes(1)
 
